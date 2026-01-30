48e7c9ecd8c18599809c94f70b70e70f7a2bfe29
Martin Kochadampally <martin@Martins-MacBook-Pro.local>
2026-01-30 15:37:26 -0600

    I ran in to a bug when trying to run my code. It was returning -1 because I accidently put a ! mark before the function name in the if statment.

--- src/main.c
+++ src/main.c
@@ -1,21 +1,24 @@
+#include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
 
 #define HEIGHT 21
 #define WIDTH 80
 
-int intitialize_map(char *map[HEIGHT][WIDTH]);
+int initialize_map(char map[HEIGHT][WIDTH]);
 
 int main(int argc, char *argv[]) {
-    char *map[HEIGHT][WIDTH];
+    char map[HEIGHT][WIDTH];
 
     srand(time(NULL));
-
-    if (!initialize_map(&map))
+    printf("%c\n", map[0][0]);
+    if (initialize_map(map))
         return -1;
+    printf("%c\n", map[0][0]);
     return 0;
 }
 
-int intitialize_map(char *map[HEIGHT][WIDTH]) {
+int initialize_map(char map[HEIGHT][WIDTH]) {
+    map[0][0] = 'Z';
     return 0;
 }
