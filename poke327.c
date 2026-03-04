213c839111be40a623548fe13f96ffc5cdbd712e
Martin Kochadampally <martin@Martins-MacBook-Pro.local>
2026-03-04 12:38:12 -0600

    I have completed the code, to check the tags and get the value for how many trainers should be present.

--- poke327.c
+++ poke327.c
@@ -1,11 +1,21 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 #include <time.h>
 
 #include "queue.h"
-#include "dijikstras.h"
+#include "heap.h"
+
+
+                      //  GATE,       PATH,       SGRASS,     TGRASS,     ARCTIC,     LAKE,       FOREST,     MNTN,       PCNTR,      PMART,      BOULDER
+int cost_table[5][11] = {{10         ,10         ,10         ,20         ,25         ,__INT_MAX__,__INT_MAX__,__INT_MAX__,10         ,10         ,__INT_MAX__}, //PC
+                         {__INT_MAX__,10         ,10         ,15         ,20         ,__INT_MAX__,15         ,15         ,50         ,50         ,__INT_MAX__}, //HIKER
+                         {__INT_MAX__,10         ,10         ,20         ,25         ,__INT_MAX__,__INT_MAX__,__INT_MAX__,50         ,50         ,__INT_MAX__}, //RIVAL
+                         {__INT_MAX__,__INT_MAX__,__INT_MAX__,__INT_MAX__,__INT_MAX__,7          ,__INT_MAX__,__INT_MAX__,__INT_MAX__,__INT_MAX__,__INT_MAX__}, //SWIMMER
+                         {__INT_MAX__,10         ,10         ,20         ,25         ,__INT_MAX__,__INT_MAX__,__INT_MAX__,50         ,50         ,__INT_MAX__}};//OTHER
 
 void print_dist_map(int dist_map[HEIGHT][WIDTH]);
+int dijiksra(int dist_map[HEIGHT][WIDTH], map *m, terrain *pc_pos, enum char_type npc);
 
 /*
     % - immovable boulders and mountainous regions.
@@ -19,15 +29,38 @@ void print_dist_map(int dist_map[HEIGHT][WIDTH]);
     Ms - are Pokemarts.
     @ - Player Character
 */
+
+
 int main(int argc, char *argv[]) {
     world *w;
     queue visited;
     character *player = NULL;
+    int NUM_NPCS = 10;
     int y, x;
+    char *errmsg;
     // char input[20];
     // int new_x_pos, new_y_pos;
 
-    if (!( w = malloc(sizeof(world)))) return 1;
+    if (argc == 3 || argc == 1) {
+        if (argc == 3) {
+            if (!strcmp("--numtrainers", argv[1])) {
+                NUM_NPCS = atoi(argv[2]);
+                if (strcmp(argv[2], "0") && NUM_NPCS == 0) {
+                    errmsg = "The value of numtrainers should be an Integer.";
+                    goto ret_err;
+                }
+                if (NUM_NPCS < 0) { 
+                    errmsg = "The value of numtrainers should be greater than 0.";
+                    goto ret_err;
+                }
+            }
+        }
+    } else {
+        errmsg = "Invalid argument(s). Should be in the form: \n './poke327' or './poke327 --numtrainers <Positive Integer>'";
+        goto ret_err;
+    }
+
+    if (!( w = malloc(sizeof(world)))) return -1;
     if (world_init(w)) return -1;
 
     queue_init(&visited);
@@ -131,10 +164,10 @@ int main(int argc, char *argv[]) {
     //     }
     // }
     
-    printf("Hiker Distance Map:");
-    print_dist_map(w->hiker_dist_map); 
-    printf("Rival Distance Map:");
-    print_dist_map(w->rival_dist_map); 
+    // printf("Hiker Distance Map:");
+    // print_dist_map(w->hiker_dist_map); 
+    // printf("Rival Distance Map:");
+    // print_dist_map(w->rival_dist_map); 
 
 
     while (!queue_dequeue(&visited, &x, &y)) {
@@ -149,6 +182,142 @@ int main(int argc, char *argv[]) {
     world_destroy(w);
 
     free(w);
+
+    return 0;
+
+    ret_err:
+        printf("Error: %s\n", errmsg);
+        return -1;
+}
+
+
+/* 
+    We are just implementing dijikstra's algorithm here, so that npc 
+    can know the shortest path to get to the Player Character. 
+*/
+int dijiksra(int dist_map[HEIGHT][WIDTH], map *m, terrain *pc_pos, enum char_type npc) {
+    heap h;
+    terrain *t;
+    int y, x;
+    int size;
+    npc %= 10;
+    int cost, new_cost;
+
+    heap_init(&h);
+
+    // Sets all squares to a weight of infinity and adds the
+    // player's square to the map.
+    for (y = 0; y < HEIGHT; y++) {
+        for (x = 0; x < WIDTH; x++) {
+            if ((pc_pos)->x == x && (pc_pos)->y == y) {
+                heap_insert(&h, &m->t[y][x], 0);
+                dist_map[y][x] =  0;
+                continue;
+            }
+            dist_map[y][x] =  __INT_MAX__;
+        }
+    }
+
+    // Extracts the current min and adds its neighbours to the heap, or 
+    // decreases the key of the node if it is already in the heap.
+    while (!heap_size(&h, &size) && size) {
+        heap_extract_min(&h, &t, &cost);
+
+        y = t->y;
+        x = t->x;
+
+        if (t->NW && cost_table[npc][t->NW->val] != __INT_MAX__) {
+            new_cost = cost + cost_table[npc][t->NW->val];
+            if (new_cost < dist_map[y-1][x-1]) {
+                dist_map[y-1][x-1] = new_cost;
+                if (is_in_heap(&h, t->NW)) {
+                    decrease_key(&h, t->NW, new_cost);
+                } else {
+                    heap_insert(&h, t->NW, new_cost);
+                }
+            }
+        }
+        if (t->N && cost_table[npc][t->N->val] != __INT_MAX__) {
+            new_cost = cost + cost_table[npc][t->N->val];
+            if (new_cost < dist_map[y-1][x]) {
+                dist_map[y-1][x] = new_cost;
+                if (is_in_heap(&h, t->N)) {
+                    decrease_key(&h, t->N, new_cost);
+                } else {
+                    heap_insert(&h, t->N, new_cost);
+                }
+            }
+        }
+        if (t->NE && cost_table[npc][t->NE->val] != __INT_MAX__) {
+            new_cost = cost + cost_table[npc][t->NE->val];
+            if (new_cost < dist_map[y-1][x+1]) {
+                dist_map[y-1][x+1] = new_cost;
+                if (is_in_heap(&h, t->NE)) {
+                    decrease_key(&h, t->NE, new_cost);
+                } else {
+                    heap_insert(&h, t->NE, new_cost);
+                }
+            }
+        }
+        if (t->E && cost_table[npc][t->E->val] != __INT_MAX__) {
+            new_cost = cost + cost_table[npc][t->E->val];
+            if (new_cost < dist_map[y][x+1]) {
+                dist_map[y][x+1] = new_cost;
+                if (is_in_heap(&h, t->E)) {
+                    decrease_key(&h, t->E, new_cost);
+                } else {
+                    heap_insert(&h, t->E, new_cost);
+                }
+            }
+        }
+        if (t->SE && cost_table[npc][t->SE->val] != __INT_MAX__) {
+            new_cost = cost + cost_table[npc][t->SE->val];
+            if (new_cost < dist_map[y+1][x+1]) {
+                dist_map[y+1][x+1] = new_cost;
+                if (is_in_heap(&h, t->SE)) {
+                    decrease_key(&h, t->SE, new_cost);
+                } else {
+                    heap_insert(&h, t->SE, new_cost);
+                }
+            }
+        }
+        if (t->S && cost_table[npc][t->S->val] != __INT_MAX__) {
+            new_cost = cost + cost_table[npc][t->S->val];
+            if (new_cost < dist_map[y+1][x]) {
+                dist_map[y+1][x] = new_cost;
+                if (is_in_heap(&h, t->S)) {
+                    decrease_key(&h, t->S, new_cost);
+                } else {
+                    heap_insert(&h, t->S, new_cost);
+                }
+            }
+        }
+        if (t->SW && cost_table[npc][t->SW->val] != __INT_MAX__) {
+            new_cost = cost + cost_table[npc][t->SW->val];
+            if (new_cost < dist_map[y+1][x-1]) {
+                dist_map[y+1][x-1] = new_cost;
+                if (is_in_heap(&h, t->SW)) {
+                    decrease_key(&h, t->SW, new_cost);
+                } else {
+                    heap_insert(&h, t->SW, new_cost);
+                }
+            }
+        }
+        if (t->W && cost_table[npc][t->W->val] != __INT_MAX__) {
+            new_cost = cost + cost_table[npc][t->W->val];
+            if (new_cost < dist_map[y][x-1]) {
+                dist_map[y][x-1] = new_cost;
+                if (is_in_heap(&h, t->W)) {
+                    decrease_key(&h, t->W, new_cost);
+                } else {
+                    heap_insert(&h, t->W, new_cost);
+                }
+            }
+        }
+    }
+
+    // Recursively destroys the heap.
+    heap_destroy(&h);
 
     return 0;
 }
